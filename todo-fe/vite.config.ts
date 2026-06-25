bb54d053aad5f13433284518c38f2750a9ccb4e2
hoangduonng <nguuyen1801@gmail.com>
2026-06-26 02:54:58 +0700

    chore(opt): optimize build output

--- todo-fe/vite.config.ts
+++ todo-fe/vite.config.ts
@@ -10,4 +10,19 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("@radix-ui") || id.includes("@dnd-kit")) {
+              return "ui-core";
+            }
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
